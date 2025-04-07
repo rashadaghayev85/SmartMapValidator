ced02907a589bea52e24cb77911de4d33a420226
rashadaghayev85 <rashadra@code.edu.az>
2025-04-07 16:04:19 +0400

    update 1.2.0 version

--- SmartMapValidator/SmartMapValidator/Core/Validator.cs
+++ SmartMapValidator/SmartMapValidator/Core/Validator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SmartMapValidator.Core
@@ -12,67 +13,95 @@ namespace SmartMapValidator.Core
         public static List<string> Validate<T>(T dto)
         {
             var validationErrors = new List<string>();
-
-            // DTO sinifinin property-lərini əldə et
             var properties = typeof(T).GetProperties();
 
             foreach (var property in properties)
             {
-                // Required validation üçün nəzərdə tutulmuş xüsusi atributları tap
+                var value = property.GetValue(dto);
+
+                // Required
                 var requiredAttribute = property.GetCustomAttribute<RequiredAttribute>();
                 if (requiredAttribute != null)
                 {
-                    var value = property.GetValue(dto);
                     if (value == null || (value is string str && string.IsNullOrWhiteSpace(str)))
-                    {
-                        validationErrors.Add($"{property.Name} this field can't be empty ");
-                    }
+                        validationErrors.Add($"{property.Name} this field can't be empty");
                 }
 
-                // Range validation
+                // Range
                 var rangeAttribute = property.GetCustomAttribute<RangeAttribute>();
-                if (rangeAttribute != null)
+                if (rangeAttribute != null && value is IComparable comparable)
                 {
-                    var value = property.GetValue(dto);
-                    if (value is IComparable)
-                    {
-                        var minValue = (IComparable)rangeAttribute.Minimum;
-                        var maxValue = (IComparable)rangeAttribute.Maximum;
-
-                        if (value is IComparable valueComparable)
-                        {
-                            if (valueComparable.CompareTo(minValue) < 0 || valueComparable.CompareTo(maxValue) > 0)
-                            {
-                                validationErrors.Add($"The {property.Name} field must be between {rangeAttribute.Minimum} and {rangeAttribute.Maximum}.");
-                            }
-                        }
-                    }
+                    if (comparable.CompareTo((IComparable)rangeAttribute.Minimum) < 0 ||
+                        comparable.CompareTo((IComparable)rangeAttribute.Maximum) > 0)
+                        validationErrors.Add($"The {property.Name} field must be between {rangeAttribute.Minimum} and {rangeAttribute.Maximum}.");
                 }
 
-                // Digər validations (məsələn, regex validation, length validation və s.) əlavə oluna bilər
+                // MaxLength
+                var maxLengthAttribute = property.GetCustomAttribute<MaxLengthAttribute>();
+                if (maxLengthAttribute != null && value is string strVal && strVal.Length > maxLengthAttribute.Length)
+                    validationErrors.Add($"{property.Name} cannot exceed {maxLengthAttribute.Length} characters.");
+
+                // MinLength
+                var minLengthAttribute = property.GetCustomAttribute<MinLengthAttribute>();
+                if (minLengthAttribute != null && value is string strMin && strMin.Length < minLengthAttribute.Length)
+                    validationErrors.Add($"{property.Name} must have at least {minLengthAttribute.Length} characters.");
+
+                // Nullable
+                var nullableAttribute = property.GetCustomAttribute<NullableAttribute>();
+                if (nullableAttribute != null && value == null)
+                    validationErrors.Add($"{property.Name} can be null."); // Bu əslində error deyil, info xarakterlidir, istəsən çıxara bilərsən.
+
+                // Regex
+                var regexAttribute = property.GetCustomAttribute<RegexAttribute>();
+                if (regexAttribute != null && value is string regexStr && !Regex.IsMatch(regexStr, regexAttribute.Pattern))
+                    validationErrors.Add($"{property.Name} is not in the correct format.");
             }
 
             return validationErrors;
         }
     }
 
-    // Custom Required validation atributu
-    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
-    public sealed class RequiredAttribute : Attribute
-    {
-    }
+    // Atributlar eyni qalır
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class RequiredAttribute : Attribute { }
 
-    // Custom Range validation atributu
-    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Property)]
     public sealed class RangeAttribute : Attribute
     {
         public object Minimum { get; }
         public object Maximum { get; }
 
-        public RangeAttribute(object minimum, object maximum)
+        public RangeAttribute(object min, object max)
         {
-            Minimum = minimum;
-            Maximum = maximum;
+            Minimum = min;
+            Maximum = max;
         }
     }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class MaxLengthAttribute : Attribute
+    {
+        public int Length { get; }
+
+        public MaxLengthAttribute(int length) => Length = length;
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class MinLengthAttribute : Attribute
+    {
+        public int Length { get; }
+
+        public MinLengthAttribute(int length) => Length = length;
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class RegexAttribute : Attribute
+    {
+        public string Pattern { get; }
+
+        public RegexAttribute(string pattern) => Pattern = pattern;
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class NullableAttribute : Attribute { }
 }
